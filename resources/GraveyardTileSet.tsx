73695800e12b6e1556167082d48484c1f94e3d1a
William Lohan <william.g.lohan@gmail.com>
2024-10-06 13:18:42 -0700

    map work

--- resources/GraveyardTileSet.tsx
+++ resources/GraveyardTileSet.tsx
@@ -1,4 +1,16 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="GraveyardTileSet" tilewidth="16" tileheight="16" tilecount="256" columns="16">
  <image source="GraveyardTileSet.png" width="256" height="256"/>
+ <tile id="128">
+  <animation>
+   <frame tileid="128" duration="180"/>
+   <frame tileid="129" duration="180"/>
+   <frame tileid="130" duration="180"/>
+   <frame tileid="131" duration="180"/>
+   <frame tileid="132" duration="180"/>
+   <frame tileid="133" duration="180"/>
+   <frame tileid="134" duration="180"/>
+   <frame tileid="135" duration="180"/>
+  </animation>
+ </tile>
 </tileset>
